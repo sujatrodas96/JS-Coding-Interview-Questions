89977f4a2617741f561d11e103260eaa87dfc8c8
sujatro das <sujatro.das96@gmail.com>
2025-12-10 19:23:34 +0530

    new way add in longestword code

--- longestword.js
+++ longestword.js
@@ -1,3 +1,5 @@
+//Q2. Write a JavaScript function that takes a string as input and returns the longest word in the string.
+
 function longestWord(str) {
     let words = str.split(' ');
     let longest = "";
